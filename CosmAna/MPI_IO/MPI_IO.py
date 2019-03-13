8e7996ebabe4a803bae2cae8c1f8147a0db69a34
POFK <maotianxiang@bao.ac.cn>
2019-03-13 11:23:22 +0800

    feat(MPI_IO): add high level api for MPI_IO

--- CosmAna/MPI_IO/MPI_IO.py
+++ CosmAna/MPI_IO/MPI_IO.py
@@ -13,7 +13,7 @@ class MPI_IO():
     def MPI_READ(self, path=''):
         rmode = self.MPI.MODE_RDONLY
         fp = self.MPI.File.Open(self.comm, path, rmode)
-#       self.data = np.empty([N * N * N / self.size], dtype=dtype)
+#       self.datar = np.empty([N * N * N / self.size], dtype=dtype)
         offset = self.comm.Get_rank() * self.datar.nbytes
         fp.Read_at_all(offset, self.datar)
         fp.Close()
@@ -25,17 +25,54 @@ class MPI_IO():
         fp.Write_at_all(offset, self.dataw)
         fp.Close()
 
+def fromfile(path, comm, MPI, shape=[], dtype=np.float32):
+    """MPI read routine for binary file.
+
+    :path: TODO
+    :comm: TODO
+    :MPI: TODO
+    :shape: [x,y,z]
+    :dtype: TODO
+    :returns: TODO
+
+    """
+    mpi_io = MPI_IO(comm=comm, MPI=MPI)
+    l = np.array(shape).prod()
+    assert shape[0]%mpi_io.size==0, \
+            "processes number: %d, shape: %s"%(mpi_io.size, shape)
+    mpi_io.datar = np.empty([l/mpi_io.size], dtype=dtype)
+    mpi_io.MPI_READ(path=path)
+    return mpi_io.datar.copy()
+
+def tofile(data, path, comm, MPI):
+    """MPI write routine for binary file.
+
+    :data: TODO
+    :path: TODO
+    :comm: TODO
+    :MPI: TODO
+    :shape: [x,y,z]
+    :dtype: TODO
+    :returns: TODO
+
+    """
+    mpi_io = MPI_IO(comm=comm, MPI=MPI)
+    mpi_io.dataw = data.copy(order='C')
+    return mpi_io.MPI_WRITE(path=path)
+
+
 
 if __name__ == '__main__':
     from mpi4py import MPI
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
-#   from test import MPI_IO
-    mpi_io = MPI_IO(comm=comm, MPI=MPI)
-    mpi_io.dataw = np.arange(10, dtype=np.int32)
-    mpi_io.MPI_WRITE(path='./test.bin')
-    mpi_io.datar = np.empty([2, 5], dtype=np.int32)
-    mpi_io.MPI_READ(path='./test.bin')
+
+    data = np.arange(12, dtype=np.float32)
+    data = np.array_split(data, size)[rank]
+    tofile(data, '/tmp/test.bin', comm, MPI)
     if rank == 0:
-        print [size, mpi_io.datar]
+        s = np.fromfile('/tmp/test.bin', dtype=np.float32, count=12)
+        print s
+    datar = fromfile('/tmp/test.bin', comm, MPI, shape=[12], dtype=np.float32)
+    print rank, datar
